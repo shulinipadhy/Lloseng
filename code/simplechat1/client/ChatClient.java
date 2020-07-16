815143c2a822f19fa4b17a822f8a38be7dc6dab8
Shulini Padhy <spadh052@uottawa.ca>
2020-07-15 23:20:30 -0400

    Edited E7 c); the command of #login was not working before, fixed the switch-case statement

--- code/simplechat1/client/ChatClient.java
+++ code/simplechat1/client/ChatClient.java
@@ -26,11 +26,7 @@ public class ChatClient extends AbstractClient
    * the display method in the client.
    */
   ChatIF clientUI; 
-  
-  // E7 a)
-  // add login Id 
-  String loginId;
-  
+    
   //Constructors ****************************************************
   
   /**
@@ -41,32 +37,27 @@ public class ChatClient extends AbstractClient
    * @param clientUI The interface type variable.
    */
   
-  // add loginId into constructor
-  public ChatClient(String loginId, String host, int port, ChatIF clientUI) 
+  // E7 a)
+  // add loginID into constructor
+  public ChatClient(String loginID, String host, int port, ChatIF clientUI) 
     throws IOException 
   {
     super(host, port); //Call the superclass constructor
-    this.loginId = loginId;
     this.clientUI = clientUI;
     openConnection();
     // login for client to connect to server
-    handleMessageFromClientUI("#login: " + loginId);
+    this.sendToServer("#login: " + loginID);
 
-    // mandatory loginId, otherwise clinet should quit if not provided
-    if(loginId == null) {
-      clientUI.display("LoginId mandatory! Client is quitting.");
-      quit();
-    }
+    // mandatory loginID, otherwise clinet should quit if not provided
+    //if(loginID == null) {
+      //clientUI.display("LoginID mandatory! Client is quitting.");
+      //quit();
+    //}
   }
 
   
   //Instance methods ************************************************
-    
-  // method to get login Id
-  public String getLoginId() {
-    return loginId;
-  }
-
+  
   /**
    * This method handles all data that comes in from the server.
    *
@@ -86,7 +77,7 @@ public class ChatClient extends AbstractClient
 
   public void handleMessageFromClientUI(String message)
   {
-    try
+    /*try
     {
       sendToServer(message);
     }
@@ -94,7 +85,7 @@ public class ChatClient extends AbstractClient
     {
       clientUI.display("Could not send message to server.  Terminating client.");
       quit();
-    }
+    } */
 
     // E6 a)
     // Add #quit, #login, #logoff, #sethost<host>, #setport<port>
@@ -107,78 +98,104 @@ public class ChatClient extends AbstractClient
     char[] messageCharSplit = message.toCharArray();
     // each command should start with symbol #
     // so we check if it's a # command or not 
-      if(messageCharSplit[0] == ('#')) {
-        // use switch-case statement
-        switch(messageStringSplit[0]) {
-          
-          // #quit case
-          case "#quit":
-            quit();
+    if(messageCharSplit[0] == ('#')) {
+      // use switch-case statement
+      switch(messageStringSplit[0]) {
+        
+        // #quit case
+        case "#quit":
+          System.out.println("The server is quitting!");
+          quit();
+          break;
+        
+        // #logoff case
+        case "#logoff":
+          // just in case there is a connection exception
+          // use closeConnection method
+          try {
+            System.out.println("Connection is closing!");
+            closeConnection();
+          }
+          // catch the exception
+          catch(IOException exception) {
+            System.out.println("Exception error while logging off!");
             break;
-          
-          // #logoff case
-          case "#logoff":
-            // just in case there is a connection exception
-            // use closeConnection method
-            try {
-              closeConnection();
-            }
-            // catch the exception
-            catch(IOException exception) {
-              System.out.println("Exception error while logging off!");
-              break;
-            }
-          
-          // #sethost <host> case
-          case "#sethost":
-            // check if client is connected 
-            if(isConnected() == false) {
-              // if not, then use the host argument as sethost
-              this.setHost(messageStringSplit[1]);
-            }
-            else {
-              // unable to set port while connected
-              System.out.println("Unable to set host while connected!");
-            }
-            break;
+          }
 
-          // #setport <port> case
-          case "#setport":
-            // check if client is connected 
-            if(isConnected() == false) {
-              // if not, then use the port argument as setport
-              this.setPort(Integer.parseInt(messageStringSplit[1]));
-            }
-            else {
-              // unable to set port while connected
-              System.out.println("Unable to set port while connected!");
-            }
-            break;
+        // #login case
+        case "#login":
+          try {
+            openConnection();
+            System.out.println("The connection is open!");
+          }
 
-          // #gethost case
-          case "#gethost":
-            // use getHost() method from AbstractClient class
-            System.out.println("Host: " + this.getHost());
-            break;
+          catch(IOException exception) {
+            System.out.println("Error: unable to connect!");
+          }
+          break;
+        
+        // #sethost <host> case
+        case "#sethost":
+          // check if client is connected 
+          if(isConnected() == false) {
+            // if not, then use the host argument as sethost
+            this.setHost(messageStringSplit[1]);
+            System.out.println("New host is set!");
+          }
+          else {
+            // unable to set port while connected
+            System.out.println("Unable to set host while connected!");
+          }
+          break;
 
-          // #getport case
-          case"#getport":
-            // use getPort() method from AbstractClient class
-            System.out.println("Port: " + this.getPort());
-            break;
+        // #setport <port> case
+        case "#setport":
+          // check if client is connected 
+          if(isConnected() == false) {
+            // if not, then use the port argument as setport
+            this.setPort(Integer.parseInt(messageStringSplit[1]));
+            System.out.println("New port is set!");
+          }
+          else {
+            // unable to set port while connected
+            System.out.println("Unable to set port while connected!");
+          }
+          break;
 
-          default:
-            System.out.println("ERROR - No login ID specified. Connection aborted.");
-            break;
-        }
+        // #gethost case
+        case "#gethost":
+          // use getHost() method from AbstractClient class
+          System.out.println("Host: " + getHost());
+          break;
+
+        // #getport case
+        case"#getport":
+          // use getPort() method from AbstractClient class
+          System.out.println("Port: " + getPort());
+          break;
+
+        default:
+          System.out.println("The command is invalid.");
+          break;
       }
+    }
+
+    else {
+      try {
+        sendToServer(message);
+      }
+      
+      catch(IOException e) {
+        clientUI.display("Could not send message to server.  Terminating client.");
+        quit();
+      }
+    }
   }
   
   // E5 a)
   // methods from Abstract Client 
   public void connectionClosed() {
     clientUI.display("The connection of the server is closed!");
-    quit();
   }
 
   public void connectionException(Exception exception) {
